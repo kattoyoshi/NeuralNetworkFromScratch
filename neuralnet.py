c0d57e25cba48b066f37d8021fdac4714a5cd987
yoshiyuki <kato.yoshiyuki854@gmail.com>
2018-09-14 05:54:41 +0900

    Update "neuralnet.py" and "utils.py".

--- neuralnet.py
+++ neuralnet.py
@@ -1,5 +1,6 @@
 # coding utf-8
 import numpy as np
+import os
 
 class MLP_MNIST(object):
     """
@@ -7,20 +8,20 @@ class MLP_MNIST(object):
     Notification: one-hot labels are required
     """
 
-    def __init__(self, input_nodes, h1_nodes, h2_nodes, output_nodes, eta=0.1):
+    def __init__(self, input_nodes, h1_nodes, h2_nodes, output_nodes, learning_rate=0.1):
         """
         # Arguments
-            input_nodes: The bumber of the features in the input data
+            input_nodes: The number of the features in the input data
             h1_nodes: The number of the nodes in the hidden layer 1
             h2_nodes: The number of the nodes in the hidden layer 2
             output_nodes: The number of the nodes in the output layer
-            eta: learning rate
+            learning_rate: learning rate
         """
         self.input_nodes = input_nodes
         self.h1_nodes = h1_nodes
         self.h2_nodes = h2_nodes
         self.output_nodes = output_nodes
-        self.eta = eta
+        self.learning_rate = learning_rate
         
         # initialize weight & bias
         # input to hidden_1
@@ -37,93 +38,112 @@ class MLP_MNIST(object):
 
     def train(self, X, y):
         """Update trainable parameters using backpropagation"""
-        hidden_output, final_output = self.forwardpass_train(X)
-        dW1, db1, dW2, db2 = self.backpropagation(X,y,hidden_output, final_output)
-        self.update_weights(dW1, db1, dW2, db2)
+        h1_input, h1_output, h2_input, h2_output, final_output = self.__forwardpass_train(X)
+        dW1, db1, dW2, db2, dW3, db3 = self.__backpropagation(X, y, h1_input, h1_output, h2_input, h2_output, final_output)
+        self.__update_weights(dW1, db1, dW2, db2, dW3, db3)
     
-    def forwardpass_train(self, X):
+    def __forwardpass_train(self, X):
+        """Make forward pass and return the computation results for backpropagation"""
         # hidden_1
         h1_input = np.dot(X, self.W1) + self.b1
-        h1_output = self.relu_func(h1_input)
+        h1_output = self.__relu(h1_input)
         # hidden_2
         h2_input = np.dot(h1_output, self.W2) + self.b2
-        h2_output = self.relu_func(h2_input)
+        h2_output = self.__relu(h2_input)
         # output
         o_input = np.dot(h2_output, self.W3) + self.b3
-        final_output = self.softmax_func(o_input)
-        return h1_output, h2_output, final_output
+        final_output = self.__softmax(o_input)
+        return h1_input, h1_output, h2_input, h2_output, final_output
     
-    def backpropagation(self, X, y, h1_output, h2_output, final_output):
-        
+    def __backpropagation(self, X, y, h1_input, h1_output, h2_input, h2_output, final_output):
+        """Backpropagate the loss and calculate the partial derivative of the trainable parameters"""
         batch_size = y.shape[0]
-        
-        error = self.cross_entropy_loss(y, final_output)
-        output_error_term = error * 1
-        
-        db2 = np.sum(output_error_term, axis = 0) / batch_size
-        dW2 = np.dot(hidden_output.T, output_error_term) / batch_size
-        hidden_error = np.dot(output_error_term, self.W2.T)
-        hidden_error_term = (1 - hidden_output) * hidden_output * hidden_error
-        
-        db1 = np.sum(hidden_error_term, axis=0) / batch_size
-        dW1 = np.dot(X.T, hidden_error_term) / batch_size
-        
-        return dW1, db1, dW2, db2
-    
-    def update_weights(self, dW1, db1, dW2, db2, dW3, db3):
-        self.W1 += dW1
-        self.b1 += db1
-        self.W2 += dW2
-        self.b2 += db2
-        self.W3 += dW3
-        self.b3 += db3
 
-    ###### Inference ######
+        output_error_term = self.__softmax_cross_entropy_loss(y, final_output)
+        db3 = np.sum(output_error_term, axis = 0) / batch_size
+        dW3 = np.dot(h2_output.T, output_error_term) / batch_size
+
+        h2_error_term = np.dot(output_error_term, self.W2.T) * self.__relu_derivative(h2_input) 
+        db2 = np.sum(h2_error_term, axis=0) / batch_size
+        dW2 = np.dot(h1_output.T, h2_error_term) / batch_size
+        
+        h1_error_term = np.dot(h2_error_term, self.W1.T) * self.__relu_derivative(h1_input)       
+        db1 = np.sum(h1_error_term, axis=0) / batch_size
+        dW1 = np.dot(h1_output.T, h1_error_term) / batch_size
+        
+        return dW1, db1, dW2, db2, dW3, db3
+    
+    def __update_weights(self, dW1, db1, dW2, db2, dW3, db3):
+        self.W1 += -self.learning_rate * dW1
+        self.b1 += -self.learning_rate * db1
+        self.W2 += -self.learning_rate * dW2
+        self.b2 += -self.learning_rate * db2
+        self.W3 += -self.learning_rate * dW3
+        self.b3 += -self.learning_rate * db3
+
+    ###### Predict ######
 
     def predict_proba(self, X):
         # hidden_1
         h1_input = np.dot(X, self.W1) + self.b1
-        h1_output = self.relu_func(h1_input)
+        h1_output = self.__relu(h1_input)
         # hidden_2
         h2_input = np.dot(h1_output, self.W2) + self.b2
-        h2_output = self.relu_func(h2_input)
+        h2_output = self.__relu(h2_input)
         # output
         o_input = np.dot(h2_output, self.W3) + self.b3
-        y_hat = self.softmax_func(o_input)
+        y_hat = self.__softmax(o_input)
         return y_hat
     
     def predict_label(self, X):
         prob = self.predict_proba(X)
         return np.argmax(prob)
 
-    ################
-    
-    def relu_func(self, X):
-        return np.maximum(0, X)
-    
-    def softmax_func(self, X):
-        const = np.max(X)
-        exp_X = np.exp(X - const)
-        return exp_X / np.sum(exp_X)
+    ###### Save/Load ######
+
+    def save_model(self, dir_path):
+        """store parameters as .npz format"""
+        np.savez(dir_path + os.path.sep + "weights.npz", 
+                 W1=self.W1, W2=self.W2, W3=self.W3)
+        np.savez(dir_path + os.path.sep + "biases.npz",
+                 b1=self.b1, b2=self.b2, b3=self.b3)
+
+    def load_model(self, dir_path):
+        """load parameters from .npz file"""
+        weights = np.load(dir_path + os.path.sep + "weights.npz")
+        biases = np.load(dir_path + os.path.sep + "biases.npz")
+        self.W1 = weights["w1"]
+        self.W2 = weights["w2"]
+        self.W3 = weights["w3"]
+        self.b1 = biases["b1"]
+        self.b2 = biases["b2"]
+        self.b3 = biases["b3"]
+
+    ##### Metrix #####
     
     def cross_entropy_loss(self, y, y_hat):
         delta = 1e-7
         return -np.sum(y_hat * np.log(y + delta))
+
+    ##### Activation function
     
+    def __relu(self, X):
+        """Relu function"""
+        X_ = np.copy(X)
+        return np.maximum(0, X)
     
+    def __relu_derivative(self, X):
+        """"Derivative of the relu function"""
+        X_ = np.zeros_like(X)
+        X_[X >= 0] = 1
+        return X_
 
-
-def main():
-    # load iris dataset
-    iris_data = datasets.load_iris()
-    print(iris_data.keys())
-    #print(iris_data)
-
-
-def TwoLayerNet():
-    pass
-
-
-if __name__ == '__main__':
-    main()
-
+    def __softmax(self, X):
+        """"Softmax function"""
+        const = np.max(X)
+        exp_X = np.exp(X - const)
+        return exp_X / np.sum(exp_X)
+    
+    def __softmax_cross_entropy_loss(self, y, y_hat):
+        """Output error term, in case Activation=softmax and Loss=cross entropy"""
+        return -(y- y_hat)
--- utils.py
+++ utils.py
@@ -1,20 +1,77 @@
-#%%
+# coding utf-8
 import numpy as np
-from sklearn import datasets
-import pandas as pd
-
-#%%
-def main():
-    # load iris dataset
-    iris_data = datasets.load_iris()
-    print(iris_data.keys())
-    #print(iris_data)
+from sklearn.preprocessing import OneHotEncoder
+import os
+import gzip
 
 
-def TwoLayerNet():
-    pass
+def batch_generator(X, y, batch_size=32, shuffle=True):
+    n_data = len(X)
+    index = np.arange(len(y))
+    if shuffle:
+        index = np.random.shuffle(index)
+    
+    n_loop = n_data // batch_size
+    n_last_batch = n_data % batch_size
+    if n_last_batch != 0:
+        n_loop += 1
+    
+    for ii in range(0, n_data, batch_size):
+        if n_data - ii < batch_size:
+            features = X[index[ii:]]
+            targets = y[index[ii:]]
+        else:
+            features = X[index[ii:ii+batch_size]]
+            targets = y[index[ii:ii+batch_size]]
+        yield features, targets
 
 
-if __name__ == '__main__':
-    main()
+class mnist_dataset():
+    
+    def __init__(self, dir_path):
+        
+        assert os.path.exists(dir_path), "Arguments error: dir_path does not exist"
+
+        # store path of the data directory
+        self.dir_path = dir_path + os.sep
+        # define file name
+        self.ftrain_feature = self.dir_path + "train-images-idx3-ubyte.gz"
+        self.ftrain_labels = self.dir_path + "train-labels-idx1-ubyte.gz"
+        self.ftest_features = self.dir_path + "t10k-images-idx3-ubyte.gz"
+        self.ftest_labels = self.dir_path + "t10k-labels-idx1-ubyte.gz"
+
+        for path in [self.ftrain_feature, self.ftrain_labels, self.ftest_features, self.ftest_labels]:
+            print(path)
+            assert os.path.exists(path), "File error: " + path + " does not exist"
+    
+    def load(self):
+        X_train = self.load_features(self.ftrain_feature)
+        X_test = self.load_features(self.ftest_features)
+        y_train = self.load_labels(self.ftrain_labels)
+        y_test = self.load_labels(self.ftest_labels)
+
+        # one-hot encoding
+        y_train = self.one_hot_encoding(y_train)
+        y_test = self.one_hot_encoding(y_test)
+        
+        return X_train, X_test, y_train, y_test
+
+    def load_features(self, file_path):
+        """Load images as 1D array"""
+        with gzip.open(file_path, 'rb') as f:
+            features = np.frombuffer(f.read(), dtype=np.uint8, offset=16)
+        return features.reshape(-1, 28*28)
+    
+    def load_labels(self, file_path):
+        """Load labels as 1D array"""
+        with gzip.open(file_path, 'rb') as f:
+            labels = np.frombuffer(f.read(), dtype=np.uint8, offset=8)
+        return labels
+
+    def one_hot_encoding(self, y):
+        """Convert binary labels into one-hot encoding"""
+        y = y.reshape(1,-1)
+        y = y.transpose()
+        encoder = OneHotEncoder()
+        return encoder.fit_transform(y).toarray()
 
